be2a6fd6a3dbf8fedf7e047e884119f96362d9c1
Sheikh Ariful Islam <sheikharifulislam101@gmail.com>
2023-11-01 08:16:37 +0600

    test: rewrite testcase and testutile file

---
+++ src/util/test-utils.js
@@ -0,0 +1,31 @@
+import { Container, MantineProvider } from "@mantine/core";
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+export * from "@testing-library/react";
+
+Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: vi.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: vi.fn(), // Deprecated
+        removeListener: vi.fn(), // Deprecated
+        addEventListener: vi.fn(),
+        removeEventListener: vi.fn(),
+        dispatchEvent: vi.fn(),
+    })),
+});
+
+const AllTheProviders = ({ children }) => {
+    return (
+        <MantineProvider>
+            <Container fluid>{children}</Container>
+        </MantineProvider>
+    );
+};
+
+const customRender = (ui, options) =>
+    render(ui, { wrapper: AllTheProviders, ...options });
+
+export { customRender as render };
