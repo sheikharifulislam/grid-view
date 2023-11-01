527813a4a37ee3e9063e272fbe055f6444db546a
Sheikh Ariful Islam <sheikharifulislam101@gmail.com>
2023-11-01 08:16:05 +0600

    chore: added some configuration

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,53 @@
 import react from "@vitejs/plugin-react";
+import fs from "fs/promises";
+import * as path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    esbuild: {
+        loader: "jsx",
+        include: /src\/.*\.jsx?$/,
+        exclude: [],
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            plugins: [
+                {
+                    name: "load-js-files-as-jsx",
+                    setup(build) {
+                        build.onLoad(
+                            { filter: /src\/.*\.js$/ },
+                            async (args) => ({
+                                loader: "jsx",
+                                contents: await fs.readFile(args.path, "utf8"),
+                            })
+                        );
+                    },
+                },
+            ],
+        },
+    },
+    server: {
+        port: 3000,
+    },
+    preview: {
+        port: 3000,
+    },
+    resolve: {
+        alias: {
+            // "@": path.resolve(__dirname, "./src"),
+            src: path.resolve(__dirname, "./src"),
+            utils: path.resolve(__dirname, "./src/util/"),
+        },
+    },
     test: {
         globals: true,
         environment: "jsdom",
-        setupFiles: ["./setupTest.js"],
+        setupFiles: "./src/util/test-utils.js",
+        coverage: {
+            enabled: true,
+        },
     },
 });
