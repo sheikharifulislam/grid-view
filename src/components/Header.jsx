92f44c9f92c54e1665a9ea6349cd2c3ef9e6eb10
Sheikh Ariful Islam <sheikharifulislam101@gmail.com>
2023-11-03 09:01:30 +0600

    feat: have add image select and delete functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,37 @@
 import { Button, Checkbox, Flex } from "@mantine/core";
+import useImagesContext from "hooks/useImagesContext";
 import React from "react";
 
 const Header = () => {
+    const { selectedImages, setSelectedImages, images, setImages } =
+        useImagesContext();
+
+    const handleSelectAllImages = ({ target }) => {
+        const selectedImagesSet = new Set(selectedImages);
+        if (target.checked) {
+            images.forEach((image) => {
+                selectedImagesSet.add(image.id);
+            });
+        } else {
+            selectedImagesSet.clear();
+        }
+
+        setSelectedImages(selectedImagesSet);
+    };
+
+    const handleDeleteImages = () => {
+        const selectedImagesSet = new Set(selectedImages);
+        const filteredImages = images.filter((image) => {
+            if (selectedImagesSet.has(image.id)) {
+                selectedImagesSet.delete(image.id);
+                return false;
+            }
+            return true;
+        });
+        setSelectedImages(selectedImagesSet);
+        setImages(filteredImages);
+    };
+
     return (
         <Flex
             justify="space-between"
@@ -10,8 +40,17 @@ const Header = () => {
                 padding: "10px 15px",
             }}
         >
-            <Checkbox label="3 file selected" />
-            <Button variant="filled" color="red">
+            <Checkbox
+                label={`${new Set(selectedImages).size} File Selected`}
+                onChange={(e) => handleSelectAllImages(e)}
+                {...(selectedImages.size === 0 ? { value: false } : {})}
+            />
+            <Button
+                variant="filled"
+                color="red"
+                onClick={handleDeleteImages}
+                disabled={!selectedImages.size}
+            >
                 Delete Files
             </Button>
         </Flex>
